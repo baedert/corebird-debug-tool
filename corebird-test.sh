34d6816530c3d7318d91fd78b48be25f11a4330a
Timm Bäder <mail@baedert.org>
2015-05-23 11:58:55 +0200

    Initial commit

---
+++ corebird-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+_data=`cat $1`
+
+
+_data=${_data//\\/\\\\}
+_data=${_data/\\u003e/\>}
+_data=${_data/\\u003c/\<}
+
+echo $_data
+
+
+
+gdbus call --session --dest org.baedert.corebird --object-path /org/baedert/corebird --method org.gtk.Actions.Activate 'post-json' "[<('baedert', '$_data')>]" "{}"
