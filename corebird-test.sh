4ff8f95683105b4cbee47c9eccb8248fa9463d8a
Timm Bäder <mail@baedert.org>
2015-12-11 18:15:45 +0100

    Update

--- corebird-test.sh
+++ corebird-test.sh
@@ -7,8 +7,6 @@ _data=${_data//\\/\\\\}
 _data=${_data/\\u003e/\>}
 _data=${_data/\\u003c/\<}
 
-echo $_data
-
-
+#echo $_data
 
 gdbus call --session --dest org.baedert.corebird --object-path /org/baedert/corebird --method org.gtk.Actions.Activate 'post-json' "[<('baedert', '$_data')>]" "{}"
